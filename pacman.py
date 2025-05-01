a570fdf50e6076d5b70662eb507bd06710415ba1
cmason-pvcc <cm77240@email.vccs.edu>
2025-05-01 10:26:40 -0400

    added fruits

--- pacman.py
+++ pacman.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import os
+import random
 
 #setting variables
 playerScore = 0
@@ -9,19 +10,21 @@ black = (0, 0, 0)
 yellow = (255, 255, 0)
 blue = (0, 0, 255)
 white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
 validItemSpaces = [[(45, 45) , (75, 45) , (105, 45) , (135, 45) , (165, 45) , (195, 45) , None      , None      , (285, 45) , (315, 45) , (345, 45) , (375, 45) , (405, 45) , (435, 45)],
                   [(45, 75) , None     , None      , (135, 75) , None      , (195, 75) , None      , None      , (285, 75) , None      , (345, 75) , None      , None      , (435, 75)],
                   [(45, 105), (75, 105), (105, 105), (135, 105), None      , (195, 105), None      , None      , (285, 105), None      , (345, 105), (375, 105), (405, 105), (435, 105)],
                   [None     , None     , (105, 135), None      , (165, 135), (195, 135), (225, 135), (255, 135), (285, 135), (315, 135), None      , (375, 135), None      , None],
-                  [None     , None     , (105, 165), (135, 165), (165, 165), None      , None      , None      , None      , (315, 165), (345, 165), (375, 165), None      , None],
-                  [None     , None     , (105, 195), None      , (165, 195), (195, 195), None      , None      , (285, 195), (315, 195), None      , (375, 195), None      , None],
-                  [None     , None     , (105, 225), None      , (165, 225), (195, 225), (225, 225), (255, 225), (285, 225), (315, 225), None      , (375, 225), None      , None],
-                  [None     , None     , (105, 255), None      , (165, 255), (195, 255), None      , None      , (285, 255), (315, 255), None      , (375, 255), None      , None],
+                  [None     , None     , (105, 165), None      , (165, 165), None      , None      , None      , None      , (315, 165), None      , (375, 165), None      , None],
+                  [None     , None     , (105, 195), (135, 195), (165, 195), (195, 195), None      , None      , (285, 195), (315, 195), (345, 195), (375, 195), None      , None],
+                  [None     , None     , (105, 225), None      , None      , (195, 225), (225, 225), (255, 225), (285, 225), None      , None      , (375, 225), None      , None],
+                  [None     , None     , (105, 255), None      , None      , (195, 255), None      , None      , (285, 255), None      , None      , (375, 255), None      , None],
                   [None     , None     , (105, 285), (135, 285), (165, 285), (195, 285), (225, 285), (255, 285), (285, 285), (315, 285), (345, 285), (375, 285), None      , None],
-                  [None     , None     , (105, 315), (135, 315), (165, 315), None      , None      , None      , None      , (315, 315), (345, 315), (375, 315), None      , None],
+                  [None     , None     , (105, 315), None      , (165, 315), None      , None      , None      , None      , (315, 315), None      , (375, 315), None      , None],
                   [None     , None     , (105, 345), None      , (165, 345), (195, 345), None      , None      , (285, 345), (315, 345), None      , (375, 345), None      , None],
-                  [(45, 375), (75, 375), (105, 375), None      , (165, 375), (195, 375), (225, 375), (255, 375), (285, 375), (315, 375), None      , (375, 375), (405, 375), (435, 375)],
-                  [(45, 405), None     , None      , None      , None      , None      , (225, 405), (255, 405), None      , None      , None      , None      , None      , (435, 405)],
+                  [(45, 375), (75, 375), (105, 375), None      , None      , (195, 375), (225, 375), (255, 375), (285, 375), None      , None      , (375, 375), (405, 375), (435, 375)],
+                  [(45, 405), None     , None      , None      , None      , None      , (225, 405), None      , None      , None      , None      , None      , None      , (435, 405)],
                   [(45, 435), (75, 435), (105, 435), (135, 435), (165, 435), (195, 435), (225, 435), (255, 435), (285, 435), (315, 435), (345, 435), (375, 435), (405, 435), (435, 435)]]
 validDotSpaces = validItemSpaces
 dots = []
@@ -65,13 +68,26 @@ playerPos = startPos
 vInput = 0
 hInput = 0
 dt = 0
+showFruit = False
+fruitDelay = random.randint(2000, 5000)/1000
+fruitPos = validItemSpaces[random.randint(0, 13)][random.randint(0, 13)]
+if fruitPos == None:
+    while fruitPos == None:
+        fruitPos = validItemSpaces[random.randint(0, 13)][random.randint(0, 13)]
+fruit = [pygame.draw.rect(screen, green, (-100, 0, 1, 1))]
 
 #game loop
 while running:
     #draw objects
     screen.fill(black)
-    maze = [pygame.draw.rect(screen, blue, (0, 0, 30, 150)), pygame.draw.rect(screen, blue, (30, 0, 420, 30)), pygame.draw.rect(screen, blue, (450, 0, 30, 150)), pygame.draw.rect(screen, blue, (210, 30, 60, 90)), pygame.draw.rect(screen, blue, (30, 120, 60, 30)), pygame.draw.rect(screen, blue, (60, 150, 30, 60)), pygame.draw.rect(screen, blue, (0, 180, 60, 30)), pygame.draw.rect(screen, blue, (0, 270, 60, 30)), pygame.draw.rect(screen, blue, (60, 270, 30, 60)), pygame.draw.rect(screen, blue, (30, 330, 60, 30)), pygame.draw.rect(screen, blue, (0, 330, 30, 150)), pygame.draw.rect(screen, blue, (30, 450, 420, 30)), pygame.draw.rect(screen, blue, (450, 330, 30, 150)), pygame.draw.rect(screen, blue, (390, 330, 60, 30)), pygame.draw.rect(screen, blue, (390, 270, 30, 60)), pygame.draw.rect(screen, blue, (420, 270, 60, 30)), pygame.draw.rect(screen, blue, (420, 180, 60, 30)), pygame.draw.rect(screen, blue, (390, 150, 30, 60)), pygame.draw.rect(screen, blue, (390, 120, 60, 30)), pygame.draw.rect(screen, blue, (60, 60, 60, 30)), pygame.draw.rect(screen, blue, (150, 60, 30, 60)), pygame.draw.rect(screen, blue, (120, 120, 30, 30)), pygame.draw.rect(screen, blue, (180, 150, 120, 30)), pygame.draw.rect(screen, blue, (210, 180, 60, 30)), pygame.draw.rect(screen, blue, (120, 180, 30, 90)), pygame.draw.rect(screen, blue, (210, 240, 60, 30)), pygame.draw.rect(screen, blue, (180, 300, 120, 30)), pygame.draw.rect(screen, blue, (210, 330, 60, 30)), pygame.draw.rect(screen, blue, (120, 330, 30, 60)), pygame.draw.rect(screen, blue, (60, 390, 150, 30)), pygame.draw.rect(screen, blue, (330, 330, 30, 60)), pygame.draw.rect(screen, blue, (270, 390, 150, 30)), pygame.draw.rect(screen, blue, (330, 180, 30, 90)), pygame.draw.rect(screen, blue, (360, 60, 60, 30)), pygame.draw.rect(screen, blue, (300, 60, 30, 60)), pygame.draw.rect(screen, blue, (330, 120, 30, 30))]
+    maze = [pygame.draw.rect(screen, blue, (0, 0, 30, 150)), pygame.draw.rect(screen, blue, (30, 0, 420, 30)), pygame.draw.rect(screen, blue, (450, 0, 30, 150)), pygame.draw.rect(screen, blue, (210, 30, 60, 90)), pygame.draw.rect(screen, blue, (30, 120, 60, 30)), pygame.draw.rect(screen, blue, (60, 150, 30, 60)), pygame.draw.rect(screen, blue, (0, 180, 60, 30)), pygame.draw.rect(screen, blue, (0, 240, 60, 30)), pygame.draw.rect(screen, blue, (60, 240, 30, 90)), pygame.draw.rect(screen, blue, (30, 330, 60, 30)), pygame.draw.rect(screen, blue, (0, 330, 30, 150)), pygame.draw.rect(screen, blue, (30, 450, 420, 30)), pygame.draw.rect(screen, blue, (450, 330, 30, 150)), pygame.draw.rect(screen, blue, (390, 330, 60, 30)), pygame.draw.rect(screen, blue, (390, 240, 30, 90)), pygame.draw.rect(screen, blue, (420, 240, 60, 30)), pygame.draw.rect(screen, blue, (420, 180, 60, 30)), pygame.draw.rect(screen, blue, (390, 150, 30, 60)), pygame.draw.rect(screen, blue, (390, 120, 60, 30)), pygame.draw.rect(screen, blue, (60, 60, 60, 30)), pygame.draw.rect(screen, blue, (150, 60, 30, 60)), pygame.draw.rect(screen, blue, (120, 120, 30, 60)), pygame.draw.rect(screen, blue, (180, 150, 120, 30)), pygame.draw.rect(screen, blue, (210, 180, 60, 30)), pygame.draw.rect(screen, blue, (120, 210, 60, 60)), pygame.draw.rect(screen, blue, (210, 240, 60, 30)), pygame.draw.rect(screen, blue, (180, 300, 120, 30)), pygame.draw.rect(screen, blue, (210, 330, 60, 30)), pygame.draw.rect(screen, blue, (120, 300, 30, 90)), pygame.draw.rect(screen, blue, (60, 390, 150, 30)), pygame.draw.rect(screen, blue, (330, 300, 30, 90)), pygame.draw.rect(screen, blue, (240, 390, 180, 30)), pygame.draw.rect(screen, blue, (300, 210, 60, 60)), pygame.draw.rect(screen, blue, (360, 60, 60, 30)), pygame.draw.rect(screen, blue, (300, 60, 30, 60)), pygame.draw.rect(screen, blue, (330, 120, 30, 60)), pygame.draw.rect(screen, blue, (150, 360, 30, 30)), pygame.draw.rect(screen, blue, (300, 360, 30, 30))]
+    leftTP = pygame.draw.rect(screen, black, (0, 210, 1, 30))
+    rightTP = pygame.draw.rect(screen, black, (479, 210, 1, 30))
     DrawDots(validDotSpaces)
+    if fruitDelay <= 0:
+        showFruit = True
+    if showFruit:
+        fruit = [pygame.draw.rect(screen, white, (fruitPos[0], fruitPos[1] - 12, 2, 4)), pygame.draw.rect(screen, green, (fruitPos[0] - 6, fruitPos[1] - 10, 6, 2)), pygame.draw.rect(screen, green, (fruitPos[0] + 2, fruitPos[1] - 10, 6, 2)), pygame.draw.rect(screen, green, (fruitPos[0] - 4, fruitPos[1] - 8, 10, 2)), pygame.draw.rect(screen, green, (fruitPos[0], fruitPos[1] - 6, 2, 2)), pygame.draw.rect(screen, red, (fruitPos[0] - 10, fruitPos[1] - 6, 2, 8)), pygame.draw.rect(screen, red, (fruitPos[0] - 8, fruitPos[1] - 8, 2, 14)), pygame.draw.rect(screen, red, (fruitPos[0] - 6, fruitPos[1] - 8, 2, 16)), pygame.draw.rect(screen, red, (fruitPos[0] - 4, fruitPos[1] - 6, 4, 16)), pygame.draw.rect(screen, red, (fruitPos[0], fruitPos[1] - 4, 2, 16)), pygame.draw.rect(screen, red, (fruitPos[0] + 2, fruitPos[1] - 6, 4, 16)), pygame.draw.rect(screen, red, (fruitPos[0] + 6, fruitPos[1] - 8, 4, 14)), pygame.draw.rect(screen, red, (fruitPos[0] + 10, fruitPos[1] - 6, 2, 8)), pygame.draw.rect(screen, white, (fruitPos[0] - 8, fruitPos[1] - 4, 2, 2)), pygame.draw.rect(screen, white, (fruitPos[0] - 6, fruitPos[1] + 2, 2, 2)), pygame.draw.rect(screen, white, (fruitPos[0] - 4, fruitPos[1] - 2, 2, 2)), pygame.draw.rect(screen, white, (fruitPos[0] - 2, fruitPos[1] + 6, 2, 2)), pygame.draw.rect(screen, white, (fruitPos[0], fruitPos[1] - 2, 2, 2)), pygame.draw.rect(screen, white, (fruitPos[0], fruitPos[1] + 2, 2, 2)), pygame.draw.rect(screen, white, (fruitPos[0] + 4, fruitPos[1] - 4, 2, 2)), pygame.draw.rect(screen, white, (fruitPos[0] + 4, fruitPos[1] + 6, 2, 2)), pygame.draw.rect(screen, white, (fruitPos[0] + 6, fruitPos[1], 2, 2)), pygame.draw.rect(screen, white, (fruitPos[0] + 8, fruitPos[1] - 6, 2, 2))]
     
     oldPos = playerPos.copy()
 
@@ -98,7 +114,7 @@ while running:
     #move player
     playerPos.x += 150 * hInput * dt
     playerPos.y += 150 * vInput * dt
-    player = pygame.draw.circle(screen, yellow, playerPos, 13)
+    player = pygame.draw.circle(screen, yellow, playerPos, 14)
 
     #detect and handle dot collision
     for dot in range(len(dots)):
@@ -111,17 +127,31 @@ while running:
     if dots == []:
         running = False
 
+    if player.collidelist(fruit) != -1 and showFruit:
+        showFruit = False
+        playerScore += 50
+        fruitPos = validItemSpaces[random.randint(0, 13)][random.randint(0, 13)]
+        if fruitPos == None:
+            while fruitPos == None:
+                fruitPos = validItemSpaces[random.randint(0, 13)][random.randint(0, 13)]
+        fruitDelay = random.randint(2000, 5000)/1000        
+
+    #wall collision
     if player.collidelist(maze) != -1:
-        while player.collidelist(maze) != -1:
-            playerPos += pygame.Vector2(-1 * hInput, -1 * vInput)
-            player = pygame.draw.circle(screen, yellow, playerPos, 13)
-            print(playerPos)
+        playerPos = oldPos.copy()
         hInput = 0
         vInput = 0
+
+    #teleportation
+    if player.colliderect(leftTP):
+        playerPos = pygame.Vector2(466, 225)
+    if player.colliderect(rightTP):
+        playerPos = pygame.Vector2(14, 225)
 
     pygame.display.flip()
 
     dt = clock.tick(60) / 1000
+    fruitDelay -= dt
 
 print(playerScore)
 pygame.quit()
